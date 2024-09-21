0a92f79ab9d02f867e56240b5d0c16576d8392ae
hanson777 <154561030+hanson777@users.noreply.github.com>
2024-09-20 20:33:01 -0700

    Create functions formain math operations, also created basic layout for calculator

---
+++ script.js
@@ -0,0 +1,37 @@
+let firstNum;
+let secondNum;
+let operator;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+": 
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1,num2);
+            break;
+    }
+}
+
