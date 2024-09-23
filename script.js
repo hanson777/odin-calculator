b91c7dccc8363e3a94503f553515baa0761b11d5
hanson777 <154561030+hanson777@users.noreply.github.com>
2024-09-23 09:48:47 -0700

    Changed how inputs to calculator are handled

--- script.js
+++ script.js
@@ -7,59 +7,19 @@ let currentDisplay = document.querySelector(".current");
 let historyDisplay = document.querySelector(".history");
 let displayValue = currentDisplay.textContent;
 
+
 document.addEventListener("click", event => {
-    if(event.target.classList.contains("1")){
-        currentDisplay.textContent += "1";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("2")){
-        currentDisplay.textContent += "2";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("3")){
-        currentDisplay.textContent += "3";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("4")){
-        currentDisplay.textContent += "4";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("5")){
-        currentDisplay.textContent += "5";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("6")){
-        currentDisplay.textContent += "6";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("7")){
-        currentDisplay.textContent += "7";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("8")){
-        currentDisplay.textContent += "8";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("9")){
-        currentDisplay.textContent += "9";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("0")){
-        currentDisplay.textContent += "0";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("add")){
-        currentDisplay.textContent += " + ";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("subtract")){
-        currentDisplay.textContent += " - ";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("multiply")){
-        currentDisplay.textContent += " * ";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("divide")){
-        currentDisplay.textContent += " / ";
-        displayValue = currentDisplay.textContent;
-    } else if(event.target.classList.contains("clear")){
-        currentDisplay.textContent = "";
-        displayValue = currentDisplay.textContent;
+    if(event.target.classList.contains("number-button")){
+        currentDisplay.textContent = event.target.textContent;
+        displayValue += currentDisplay.textContent;
+    } else if(event.target.classList.contains("operator")){
+        displayValue += event.target.textContent;
     } else if(event.target.classList.contains("equals")){
-        statement = displayValue.split(" ");
-        statement[0] = Number(statement[0]);
-        statement[2] = Number(statement[2]);
+        statement = displayValue.split("");
         currentDisplay.textContent = operate(statement[1], statement[0], statement[2]);
-    }
-});
+        displayValue = "";
+    } 
+})
 
 function add(a, b){
     return a + b;
@@ -74,10 +34,13 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if(b == 0) return "Wtf";
     return a / b;
 }
 
 function operate(operator, num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
     if(operator === "+"){
         return add(num1, num2);
     } else if(operator === "-"){
